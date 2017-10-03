c385abb0a8dd365b7f12d2e381b14a983473dd8f
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-03 14:41:27 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,9 +7,7 @@ def oxford_comma(array)
     return array.join(' and ')
   else
     array.each_with_index do |fruit, index|
-      while index != array.length - 1 do
         return_string << fruit + ', '
-      end
     end
     return return_string
   end
