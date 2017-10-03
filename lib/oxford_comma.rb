cd5002b3145b91d58f0ea6acc1617a40a179b853
Jack Heseltine <jack.heseltine@gmail.com>
2017-10-03 14:32:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,16 @@
 def oxford_comma(array)
+  return_string = ''
+  
   if array.length == 1
     return array[0]
   elsif array.length == 2
     return array.join(' and ')
   else
-    
+    array.each_with_index do |fruit,index|
+      until index == array.length
+        return_string << fruit + ', '
+      end
+      return_string << ' and ' + array.last
+    end
   end
 end
